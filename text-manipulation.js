94dac4ce8ca6c379fad4fb5c638601d52e59b4bd
chris <chrisjohnnewton@gmail.com>
2021-08-17 22:11:47 +0100

    Some tweaks to the formatting of the JavaScript.

--- text-manipulation.js
+++ text-manipulation.js
@@ -84,11 +84,13 @@ function manipulateText() {
         }
     // If “Add Line Numbers” is selected.
     } else if (manipulationSelect.value === "Add Line Numbers") {
-        // Convert the input string into an array.
-        inputToArray = manipulationInput.value.split("\n");
-        // Map each array value to a new array where each item is prepended with a number, and then join the array.
-        inputWithLineNumbers = inputToArray.map((line, index) => `${index+1}. ${line}`).join("\n");
-        manipulationOutput.value = inputWithLineNumbers;
+        manipulationOutput.value = manipulationInput.value
+        // Convert the input string into an array.    
+        .split("\n")
+        // Map each array value to a new array where each item is prepended with a number.
+        .map((line, index) => `${index+1}. ${line}`)
+        // Join the array together.
+        .join("\n");
     // If “To Lowercase” is selected.
     } else if (manipulationSelect.value === "To Lowercase") {
         manipulationOutput.value = manipulationInput.value.toLowerCase()
